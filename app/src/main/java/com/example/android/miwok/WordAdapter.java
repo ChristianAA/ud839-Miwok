713c09a86fd9f6581563287120b4813b6c939aa5
ChristianAA <christian0506@gmail.com>
2017-03-25 12:48:07 +0100

    Custom ArrayAdapter

---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,44 @@
+package com.example.android.miwok;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Christian PC on 25/03/2017.
+ */
+
+public class WordAdapter extends ArrayAdapter<Word> {
+
+    public WordAdapter(Activity context, ArrayList<Word> WordAdapter) {
+        super(context, 0, WordAdapter);
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the Word object located at this position in the list
+        Word currentWord = getItem(position);
+        // Find the TextView in the list_item.xml layout with the Miwok translation
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        // Get the miwok Tranlation from the current Word object and set this text on the miwok_text_view
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+
+        // Same for defaultTextView
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        return listItemView;
+
+    }
+}
